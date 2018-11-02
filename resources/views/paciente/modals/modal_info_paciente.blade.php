dd60200498de1c4863f884b938decc27995925f4
Airton Vieira Dos Santos <airton.santos3.1415@gmail.com>
2018-11-01 22:36:12 -0300

    Criado Model Info
    Criado só model sem a estrutura JavaScript.

---
+++ resources/views/paciente/modals/modal_info_paciente.blade.php
@@ -0,0 +1,24 @@
+<!-- Modal Info-->
+<div class="modal fade" id="infoPacienteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">  
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header modal-info-css">
+        <h5 class="modal-title" id="infoPacienteLabel">Detalhes de Cadastro de Paciente</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+              <p id="infoModalNome"><strong>Nome</strong>: </p>
+              <p id="infoModalCpf"><strong>CPF</strong>: </p>
+              <p id="infoModalNasc"><strong>Data Nascimento</strong>: </p>
+              <p id="infoModalEmail"><strong>Email</strong>: </p>
+              <p id="infoModalContato"><strong>Contato</strong>: </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" id="deleteButtonModal">Update</button>
+      </div>
+    </div>
+  </div>
+</div>
